fa31e0d7932f789e73457a2364cac6cec0d6350d
Chavalit Koweerawong <chavalit.kow@gmail.com>
2023-06-09 20:09:58 +0700

    Line Message API

--- app/Models/LineMessageAPI.php
+++ app/Models/LineMessageAPI.php
@@ -8,8 +8,6 @@ use Illuminate\Http\Request;
 
 class LineMessageAPI extends Model
 {
-    public $channel_access_token = "PZdTdgSxG19zoHcgkUC6yPkkgjyS+rk5M5OLVqsBY25R91BbFclMOYVSd0EXEno4kFL0WPTdvjEsE4AK5xh6xZvA6ByMpa5xRc312kRJIwdqhd6xfDLW1nXNvxpm7NYFeto2R3LL1l6je+NGw9GQZgdB04t89/1O/w1cDnyilFU=";
-
     use HasFactory;
 
 
@@ -19,19 +17,15 @@ class LineMessageAPI extends Model
         if (count($requestData["events"]) == 0)  return;
         $event = $requestData["events"][0];
 
-
         switch ($event["message"]["type"]) {
             case "text":
-                // $this->textHandler($event);
-                // $this->replyWithText($event, "Hello World");
-                
-                $this->replyWithFlexBubble($event);
+                $this->replyWithText($event, "Hello World");                
                 break;
             case "location":
-
-                // $this->replyWithText($event, "Hello World2");
+                $this->replyWithFlexCarousel($event);
+                break;
+            case "sticker":
                 $this->replyWithFlexBubble($event);
-                // $this->locationHandler($event);
                 break;
             case "image":
                 // $this->imageHandler($event);
@@ -39,88 +33,39 @@ class LineMessageAPI extends Model
         }
     }
 
+    public  function replyWithFlexCarousel($event)
+    {
+        $template_path = storage_path('../public/flex-templates/places.json');
+        $string_json = file_get_contents($template_path);
+        $message = [
+            "type" => "flex",
+            "altText" => "This is a flex carousel template",
+            "contents" => json_decode($string_json, true),
+        ];
+        
+        $this->reply($event, $message);
+    }
     public  function replyWithFlexBubble($event)
     {
-        // $template_path = storage_path('../public/flex-templates/places.json');
         $template_path = storage_path('../public/flex-templates/bubble.json');
         $string_json = file_get_contents($template_path);
-        // $message = json_decode($string_json, true);
-        // $message = json_decode($string_json, true);
         $message = [
             "type" => "flex",
             "altText" => "This is a flex template",
             "contents" => json_decode($string_json, true),
         ];
-        $message2 = [
-            "type" => "template",
-            "altText" => "This is a carousel template",
-            "template" => json_decode($string_json, true),
-        ];
-        // $message2 = [
-        //     "type" => "text",
-        //     "text" => "HEY",
-        // ];
-
-        //GET ONLY FIRST EVENT
-        $replyToken = $event["replyToken"];
-
-        // echo $event;
-        $channel_access_token = $this->channel_access_token;
-        // $event['message'] = ['id' => ''.$data['msgocrid'] ];
-        $body = [
-            "replyToken" => $replyToken,
-            "messages" => [$message2],
-        ];
-
-        $opts = [
-            'http' => [
-                'method'  => 'POST',
-                'header'  => "Content-Type: application/json \r\n" .
-                    'Authorization: Bearer ' . $channel_access_token,
-                'content' => json_encode($body, JSON_UNESCAPED_UNICODE),
-                //'timeout' => 60
-            ]
-        ];
-
-        $context  = stream_context_create($opts);
-        //https://api-data.line.me/v2/bot/message/11914912908139/content
-        $url = "https://api.line.me/v2/bot/message/reply";
-        $result = file_get_contents($url, false, $context);
-        file_put_contents('../storage/logs/log.txt', $result . PHP_EOL, FILE_APPEND);
+        
+        $this->reply($event, $message);
     }
 
-    public  function replyImage($event, $image)
-    {
-        $message = [
-            "type" => "text",
-            "text" => $image,
-        ];
-        //GET ONLY FIRST EVENT
-        $replyToken = $event["replyToken"];
-
-        // echo $event;
-        $channel_access_token = $this->channel_access_token;
-        // $event['message'] = ['id' => ''.$data['msgocrid'] ];
-        $body = [
-            "replyToken" => $replyToken,
-            "messages" => [$message],
-        ];
-
-        $opts = [
-            'http' => [
-                'method'  => 'POST',
-                'header'  => "Content-Type: application/json \r\n" .
-                    'Authorization: Bearer ' . $channel_access_token,
-                'content' => json_encode($body, JSON_UNESCAPED_UNICODE),
-                //'timeout' => 60
-            ]
-        ];
-
-        $context  = stream_context_create($opts);
-        //https://api-data.line.me/v2/bot/message/11914912908139/content
-        $url = "https://api.line.me/v2/bot/message/reply";
-        file_get_contents($url, false, $context);
-    }
+    // public  function replyImage($event, $image)
+    // {
+    //     $message = [
+    //         "type" => "text",
+    //         "text" => $image,
+    //     ];        
+    //     $this->reply($event, $message);
+    // }
 
     public  function replyWithText($event, $text)
     {
@@ -128,11 +73,16 @@ class LineMessageAPI extends Model
             "type" => "text",
             "text" => $text,
         ];
+        $this->reply($event, $message);
+    }
+
+    public  function reply($event, $message)
+    {
         //GET ONLY FIRST EVENT
         $replyToken = $event["replyToken"];
 
         // echo $event;
-        $channel_access_token = $this->channel_access_token;
+        // $channel_access_token = $this->channel_access_token;
         // $event['message'] = ['id' => ''.$data['msgocrid'] ];
         $body = [
             "replyToken" => $replyToken,
@@ -143,7 +93,7 @@ class LineMessageAPI extends Model
             'http' => [
                 'method'  => 'POST',
                 'header'  => "Content-Type: application/json \r\n" .
-                    'Authorization: Bearer ' . $channel_access_token,
+                    'Authorization: Bearer ' . env('LINE_CHANNEL_ACCESS_TOKEN'),
                 'content' => json_encode($body, JSON_UNESCAPED_UNICODE),
                 //'timeout' => 60
             ]
