84263b5af35e49ecb563c172b1cb1d36127c9422
arc6828 <chavalit.kow@gmail.com>
2023-12-07 13:09:25 +0700

    Update medium-feed.blade.php

--- resources/views/scraping/medium-feed.blade.php
+++ resources/views/scraping/medium-feed.blade.php
@@ -6,6 +6,31 @@
             <form method="post" action="{{ route('scraping.medium-feed.store') }}">
                 @csrf
                 <div class="mb-3">
+                    <label for="url" class="form-label">URL</label>
+                    <input type="" class="form-control" id="url" name="url"required onkeyup="extract(this);">
+                    <div id="urlHelp" class="form-text">
+                        your URL on medium
+                    </div>
+                </div>
+                <script>
+                    function extract(element){
+                        // https://medium.com/cs-vru/tagged/computer-science
+                        let url = element.value;
+                        let s = url.replace("https://medium.com/","");
+                        let a = s.split("/");
+                        console.log(a);
+                        if (a.length == 1 ){
+                            document.querySelector("#publication").value = a[0];
+                            document.querySelector("#tagname").value = "";
+                        }else if (a.length == 3 ){
+                            document.querySelector("#publication").value = a[0];
+                            document.querySelector("#tagname").value = a[2];
+                        }
+                        
+
+                    }
+                </script>
+                <div class="mb-3">
                     <label for="publication" class="form-label">Publication</label>
                     <input type="" class="form-control" id="publication" name="publication"required>
                     <div id="emailHelp" class="form-text">
