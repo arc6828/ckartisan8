a88bc59fdcfc4206f5b5495a07e6697f3edf816c
arc6828 <chavalit.kow@gmail.com>
2023-12-07 17:07:26 +0700

    Update medium-feed.blade.php

--- resources/views/scraping/medium-feed.blade.php
+++ resources/views/scraping/medium-feed.blade.php
@@ -7,40 +7,48 @@
                 @csrf
                 <div class="mb-3">
                     <label for="url" class="form-label">URL</label>
-                    <input type="" class="form-control" id="url" name="url"required onkeyup="extract(this);">
+                    <input type="" class="form-control" id="url" name="url"required
+                        onkeyup="extract(this);">
                     <div id="urlHelp" class="form-text">
                         your URL on medium
                     </div>
                 </div>
                 <script>
-                    function extract(element){
+                    function extract(element) {
                         // https://medium.com/cs-vru/tagged/computer-science
                         let url = element.value;
-                        let s = url.replace("https://medium.com/","");
+                        let s = url.replace("https://medium.com/", "");
                         let a = s.split("/");
                         console.log(a);
-                        if (a.length == 1 ){
+                        if (a.length == 1) {
                             document.querySelector("#publication").value = a[0];
                             document.querySelector("#tagname").value = "";
-                        }else if (a.length == 3 ){
+                        } else if (a.length == 3) {
                             document.querySelector("#publication").value = a[0];
                             document.querySelector("#tagname").value = a[2];
                         }
-                        
+
 
                     }
                 </script>
-                <div class="mb-3">
-                    <label for="publication" class="form-label">Publication</label>
-                    <input type="" class="form-control" id="publication" name="publication"required readonly>
-                    <div id="emailHelp" class="form-text">
-                        your publication on medium
+                <div class="row mb-3">
+                    <div class="col">
+                        <label for="publication" class="form-label">Publication</label>
+                        <input type="" class="form-control bg-light" id="publication" name="publication"required
+                            readonly>
+                        <div id="emailHelp" class="form-text">
+                            your publication on medium
+                        </div>
+                    </div>
+                    <div class="col">
+                        <label for="tagname" class="form-label">Tagname (optional)</label>
+                        <input type="" class="form-control bg-light" id="tagname" name="tagname" readonly>
+                        <div id="emailHelp" class="form-text">
+                            your tag on the selected publication
+                        </div>
                     </div>
                 </div>
-                <div class="mb-3">
-                    <label for="tagname" class="form-label">Tagname (optional)</label>
-                    <input type="" class="form-control" id="tagname" name="tagname" readonly>
-                </div>
+
 
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
